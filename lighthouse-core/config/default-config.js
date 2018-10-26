c4cc7fb346032aa0f3190c96cd9708447d887701
Brendan Kenny <bckenny@gmail.com>
2018-10-25 18:10:31 -0700

    new_audit: offline-start-url

--- lighthouse-core/config/default-config.js
+++ lighthouse-core/config/default-config.js
@@ -126,6 +126,7 @@ const defaultConfig = {
     'font-display',
     'network-requests',
     'metrics',
+    'offline-start-url',
     'manual/pwa-cross-browser',
     'manual/pwa-page-transitions',
     'manual/pwa-each-page-has-url',
